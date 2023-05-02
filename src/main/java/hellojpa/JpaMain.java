2d3d26b33817c7fa1a7f6ebeb414c59a06b36172
pjnw1236 <pjnw1236@gmail.com>
2023-05-03 02:19:08 +0900

    [섹션2] JPA 시작하기 (Hello JPA - 애플리케이션 개발) JPQL을 이용해 객체 조회

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -14,8 +15,15 @@ public class JpaMain {
         tx.begin(); // 트랜잭션 시작
 
         try {
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJpa");
+//            Member findMember = em.find(Member.class, 1L);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
 
             tx.commit(); // 커밋
         } catch (Exception e) {
